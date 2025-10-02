dabdabc2a1de4b588df1c9ff1372cdf558446432
gosenx <pamossecuambe@gmail.com>
2025-10-02 18:27:39 +0200

    fix: use repository

--- iac/res_github_env.tf
+++ iac/res_github_env.tf
@@ -1,26 +1,26 @@
-data "github_organization" "org" {
-  name = "RFC-791"
+data "github_repository" "backend" {
+  name = "RFC-791/aws-ug-maputo-demo-backend"
 }
 
 resource "github_actions_organization_variable" "ecs_cluster_name" {
   variable_name           = "AWS_UG_MAPUTO_ECS_CLUSTER_NAME"
   visibility              = "selected"
   value                   = aws_ecs_cluster.cluster.name
-  selected_repository_ids = data.github_organization.org.repositories
+  selected_repository_ids = [data.github_repository.backend.repo_id]
 }
 
 resource "github_actions_organization_variable" "lb_name" {
   variable_name           = "AWS_UG_MAPUTO_LB_DNS_NAME"
   visibility              = "selected"
   value                   = aws_lb.ingress.dns_name
-  selected_repository_ids = data.github_organization.org.repositories
+  selected_repository_ids = [data.github_repository.backend.repo_id]
 }
 
 resource "github_actions_organization_variable" "lb_arn" {
   variable_name           = "AWS_UG_MAPUTO_LB_ARN"
   visibility              = "selected"
   value                   = aws_lb.ingress.arn
-  selected_repository_ids = data.github_organization.org.repositories
+  selected_repository_ids = [data.github_repository.backend.repo_id]
 }
 
 
