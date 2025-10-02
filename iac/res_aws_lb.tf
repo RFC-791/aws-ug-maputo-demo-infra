765f89993778b511ae5b1c7839e56f4a8b4fc887
gosenx <pamossecuambe@gmail.com>
2025-10-02 19:05:07 +0200

    fix: lb sg rule

--- iac/res_aws_lb.tf
+++ iac/res_aws_lb.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "lb_sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.lb_sg.id
-  cidr_ipv4         = aws_vpc.pvt.cidr_block
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
